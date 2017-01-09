d2c8019de5438a60ad649d450c7327fac1d45375
Moises Zaragoza <mZaragoza@circletechfl.com>
2017-01-08 20:35:15 -0500

    Chat

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
+
+  mount ActionCable.server => '/cable'
+
   resources :chat_rooms, only: [:new, :create, :show, :index]
   root 'chat_rooms#index'
 end
