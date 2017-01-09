1ea10402d558881c8c6137997b878a185436cd3f
Moises Zaragoza <mZaragoza@circletechfl.com>
2017-01-08 20:41:22 -0500

    updates for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,9 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://sample-cable-chat-prod.herokuapp.com',
+                                                  'http://sample-cable-chat-prod.herokuapp.com'
+                                                ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
